eb0576b94404c61f86111132fa773e64af2366d3
Lucashcr <lucash.rocha@hotmail.com>
2023-06-19 23:51:55 -0300

    Definição inicial do projeto

--- todolist/index.php
+++ todolist/index.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List</title>
+
+    <link rel="stylesheet" href="./css/base.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/favicon/site.webmanifest">
 </head>
 <body>
-    <?php echo "<h1>Hello world</h1>"; ?>
+    <?php require_once "html/base/header.html" ?>
+    <main>
+        <h1>Hello world</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, delectus molestiae? Voluptate natus doloribus consequuntur architecto facere delectus consectetur fugit quidem tenetur vitae repellendus dicta quaerat dignissimos, sit dolor ipsam.</p>
+    </main>
+    <?php require_once "html/base/footer.html" ?>
 </body>
 </html>
 
